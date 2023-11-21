8fb7903c5848b1c397d75c264d0516b5eb849254
S <sups@softwareag.com>
2023-11-22 01:05:16 +0530

    creating discussions

--- .github/actions/ApproveReview/src/index.ts
+++ .github/actions/ApproveReview/src/index.ts
@@ -73,12 +73,7 @@ async function createDiscussion() {
     const repo = github.context.repo.repo;
     const title = `Cycle Status ${github.context.runNumber} - ${github.context.runId}: [NOGO] - Review Needed `;
     const commentBody =
-      `
-      ${github.context.job} failed in the ${github.context.workflow} workflow. The status is NOGO.
-      Repository: ${repo}
-
-      @SAG-Trial/teams-1 Please review and approve the cycle run
-      `;
+      `${github.context.job} failed in the ${github.context.workflow} workflow. The status is NOGO.\nRepository: ${repo}\n@SAG-Trial/teams-1 Please review and approve the cycle run`;
 
     // const response = await octokit.rest.teams.createDiscussionInOrg({
     //   org:"SAG-Trial",
