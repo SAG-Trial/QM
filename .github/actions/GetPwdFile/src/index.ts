0a80bb64973b6f0408de493a80d0a43fbd66f602
prhsag <sups@softwareag.com>
2023-11-27 22:12:07 +0530

    getting a password by reading it's contents

---
+++ .github/actions/GetPwdFile/src/index.ts
@@ -0,0 +1,50 @@
+import {getOctokit} from "@actions/github";
+import { setFailed } from "@actions/core";
+
+const owner = "SAG-Trial";
+const repo = "b";
+
+async function readFileContents() {
+  const octokit = getOctokit(process.env.ORG_TOKEN as string);
+
+  try {
+    const pwd =await octokit.rest.repos.getContent({
+      owner,
+      repo,
+      path: "pwd.txt",
+      ref: "secret_branch"
+    });
+  
+    // console.log(Buffer.from(pwd.headers. , 'base64').toString());
+    console.log(pwd);
+  } catch (error) {
+    setFailed((error as Error).message);
+  }
+}
+
+readFileContents();
+
+
+
+// async function getFileContents(owner: string, repo: string, path: string) {
+//   const octokit = getOctokit(process.env.ORG_TOKEN as string);
+
+//   try {
+//     const response = await octokit.rest.repos.getContent({
+//       owner,
+//       repo,
+//       path,
+//     });
+
+//     if (Array.isArray(response.data)) {
+//       // Handle directory contents
+//       return response.data;
+//     } else {
+//       // Handle file contents
+//       const content = Buffer.from(response.data.content, 'base64').toString();
+//       return content;
+//     }
+//   } catch (error) {
+//     throw new Error((error as Error).message);
+//   }
+// }
