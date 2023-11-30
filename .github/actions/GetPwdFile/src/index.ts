18a28eac7b3b3777c3b151c1b15800f51d8ad92b
prhsag <sups@softwareag.com>
2023-11-30 14:42:29 +0530

    with comments

--- .github/actions/GetPwdFile/src/index.ts
+++ .github/actions/GetPwdFile/src/index.ts
@@ -1,38 +1,51 @@
+// import octokit library for making API calls to GitHub
 import { getOctokit } from "@actions/github";
 import { setFailed } from "@actions/core";
-import { getProperties } from 'properties-file'
+/* import { getProperties } from 'properties-file' */
 
+// Repo Owner Name
 const owner = "SAG-Trial";
+
+// Repo Name 
 const repo = "QM";
 
 async function readFileContents() {
   const myHeaders = new Headers();
   myHeaders.append("Accept", "application/vnd.github+json");
 
+
+  // Create an octokit instance with API token
   const octokit = getOctokit(process.env.ORG_TOKEN as string);
-  const path = "config.properties";
+
+  // file name to be read containing password
+  const path = "config.json";
 
   try {
+
+    // get the latest commit SHA for finding SubModule (IGNORE THIS)
     const headCommitSHA = await octokit.rest.repos.getCommit({
       owner,
       repo,
       ref: "main",
     });
 
+    // get the tree of the commit for getting submodule (IGNORE THIS)
     const repoDirArray = await octokit.rest.git.getTree({
       owner,
       repo,
       tree_sha: headCommitSHA.data.commit.tree.sha,
     });
 
+
+    // get the submodule name from the tree : In github submodules have type 160000 (IGNORE THIS)
     const subModuleDetails = repoDirArray.data.tree.filter(
       (item) => item.mode === "160000"
     );
 
-    // print the contents of submodule name
-    // @ts-ignore
-
+    
     try {
+
+      // Reading the contents from Submoduke repo where the password file is stored . This returns a base64 encoded string which is not practical. So we will take download_url and fetch the contents from there directly in right format
       const configContents = await octokit.rest.repos.getContent({
         owner,
         repo: subModuleDetails[0].path as string,
@@ -40,9 +53,12 @@ async function readFileContents() {
       });
 
       //@ts-ignore
-      const downloadUrl = configContents.data.download_url;
 
+      // Get download_url from configContents
+      const downloadUrl = configContents.data.download_url;
+      
       //@ts-ignore
+      //download the file from the download_url and fetch the contents as JSON
       const response = await fetch(
         downloadUrl,
         {
@@ -51,10 +67,12 @@ async function readFileContents() {
         }
       )
 
-      const passwordObject =getProperties(await response.text());
-      
+      // if password file is .properties type then use this
+      // const passwordObject =getProperties(await response.text());
 
-      // const textData = await response.json();
+
+      // if Password file is .json type then use this
+      const passwordObject = await response.json();
 
       console.log(passwordObject);
     } catch (error) {
