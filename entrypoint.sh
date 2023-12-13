d9ceeb188904fa51b3e3a2bd612d2b26cbf8f35f
prhsag <sups@softwareag.com>
2023-12-13 20:03:10 +0530

    push changes step

---
+++ entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/sh -l
+
+
+mkdir tmp
+cd tmp
+git clone https://github.com/SAG-Trial/QM.git .
+ls -la
+
+git config --global user.email "sups@softwareag.com"
+git config --global user.name "Supreeth S"
+
+gpg --import supreeth_public.asc
+
+
+git init
+git secret init
+
+git rm --cached secrets/secrets.txt
+git rm --cached -r secrets
+
+
+git secret tell sups@softwareag.com
+
+git secret add secrets/secrets.txt
+git secret hide
+
+git rm --cached secrets/secrets.txt
+git rm --cached -r secrets
+
+git add .
+git rm --cached secrets/secrets.txt
+git commit -m "Encrypted from GitHub Docker Actions"
+# git push
