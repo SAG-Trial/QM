3623e59c92543dd0266b128675a2c7809221a1e1
prhsag <sups@softwareag.com>
2023-12-13 19:56:06 +0530

    rm -r

--- .github/actions/GitSecretDocker/entrypoint.sh
+++ .github/actions/GitSecretDocker/entrypoint.sh
@@ -15,6 +15,8 @@ gpg --import supreeth_public.asc
 git init
 git secret init
 
+git rm --cached secrets/secrets.txt
+git rm --cached -r secrets
 
 
 git secret tell sups@softwareag.com
@@ -23,6 +25,7 @@ git secret add secrets/secrets.txt
 git secret hide
 
 git rm --cached secrets/secrets.txt
+git rm --cached -r secrets
 
 git add .
 git rm --cached secrets/secrets.txt
