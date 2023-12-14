85d97898cd444c4387b59e6b0c01dbbac27eba88
prhsag <sups@softwareag.com>
2023-12-14 20:25:09 +0530

    reusing ubuntu

--- .github/actions/GitSecretDocker/entrypoint.sh
+++ .github/actions/GitSecretDocker/entrypoint.sh
@@ -11,6 +11,14 @@
 # echo "Contents in revealing-folder folder"
 # ls -la
 
+
+echo "deb https://dl.bintray.com/sobolevn/deb git-secret main" | tee -a /etc/apt/sources.list
+
+wget -qO - https://api.bintray.com/users/sobolevn/keys/gpg/public.key | apt-key add -
+apt-get update && apt-get install git-secret
+
+
+
 echo "cd to github workspace"
 cd /github/workspace/
 
