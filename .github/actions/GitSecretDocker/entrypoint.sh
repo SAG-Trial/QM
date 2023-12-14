f460acfd3454876859cc9b576647621e8ba0b725
prhsag <sups@softwareag.com>
2023-12-14 20:49:16 +0530

    submodules checkout

--- .github/actions/GitSecretDocker/entrypoint.sh
+++ .github/actions/GitSecretDocker/entrypoint.sh
@@ -12,12 +12,6 @@
 # ls -la
 
 
-echo "deb https://dl.bintray.com/sobolevn/deb git-secret main" | tee -a /etc/apt/sources.list
-
-wget -qO - https://api.bintray.com/users/sobolevn/keys/gpg/public.key | apt-key add -
-apt-get update && apt-get install git-secret
-
-
 
 echo "cd to github workspace"
 cd /github/workspace/
