ced2c50bf9462af12ec663f66a57514e725e7e55
sups <sups@softwareag.com>
2024-01-04 15:51:06 +0530

    RUN pwd

--- .github/actions/NodeDockerImage/index.js
+++ .github/actions/NodeDockerImage/index.js
@@ -2,6 +2,7 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const { exec } = require("child_process");
 
+console.log("Hello World!");
 core.setOutput("myOutput", "Hello World!");
 core.setOutput("workflow", process.env.GITHUB_WORKFLOW);
 core.setOutput("githubActionPath", process.env.GITHUB_ACTION_PATH);
