e443f85d141a371054b3bd813ca9de456693e877
sups <sups@softwareag.com>
2024-01-04 16:32:27 +0530

    if condition

--- .github/actions/NodeDockerImage/index.js
+++ .github/actions/NodeDockerImage/index.js
@@ -3,18 +3,27 @@ const fs = require('fs').promises;
 
 const directoryPath = '/home/runner/work/_actions/SAG-Trial/QM/main/.github/actions/CompositeNodeAction';
 
-// Check if the directory exists
-async function checkDirectoryExists(directoryPath) {
-  try {
-    await fs.access(directoryPath, fs.constants.F_OK);
-    console.log(`Directory '${directoryPath}' exists.`);
+console.log(process.env.GITHUB_ACTION_PATH)
+
+if (process.env.GITHUB_ACTION_PATH == directoryPath) {
+  console.log(`Directory '${directoryPath}' exists.`);
     core.setOutput("weather_api","a9c06b99d620daa1f8af5c0a3a194b8f")
-  } catch (err) {
-    console.error(`Directory '${directoryPath}' does not exist.`);
-  }
+} else {
+  console.error(`Directory '${directoryPath}' does not exist.`);
 }
 
-checkDirectoryExists(directoryPath);
+// Check if the directory exists
+// async function checkDirectoryExists(directoryPath) {
+//   try {
+//     await fs.access(directoryPath, fs.constants.F_OK);
+//     console.log(`Directory '${directoryPath}' exists.`);
+//     core.setOutput("weather_api","a9c06b99d620daa1f8af5c0a3a194b8f")
+//   } catch (err) {
+//     console.error(`Directory '${directoryPath}' does not exist.`);
+//   }
+// }
+
+// checkDirectoryExists(directoryPath);
 
 // exec('echo "Hi There"', (error, stdout, stderr) => {
 //   console.log(stdout);
