5b31fe597fdcd8f87020731f597bf7c61832f285
sups <sups@softwareag.com>
2024-01-04 16:27:50 +0530

    using promises

--- .github/actions/NodeDockerImage/index.js
+++ .github/actions/NodeDockerImage/index.js
@@ -1,16 +1,20 @@
 const core = require("@actions/core");
-const fs = require('fs');
+const fs = require('fs').promises;
 
 const directoryPath = '/home/runner/work/_actions/SAG-Trial/QM/main/.github/actions/CompositeNodeAction';
 
 // Check if the directory exists
-fs.access(directoryPath, fs.constants.F_OK, (err) => {
-  if (err) {
-    console.error(`Directory '${directoryPath}' does not exist.`);
-  } else {
+async function checkDirectoryExists(directoryPath) {
+  try {
+    await fs.access(directoryPath, fs.constants.F_OK);
+    console.log(`Directory '${directoryPath}' exists.`);
     core.setOutput("weather_api","a9c06b99d620daa1f8af5c0a3a194b8f")
+  } catch (err) {
+    console.error(`Directory '${directoryPath}' does not exist.`);
   }
-});
+}
+
+checkDirectoryExists(directoryPath);
 
 // exec('echo "Hi There"', (error, stdout, stderr) => {
 //   console.log(stdout);
