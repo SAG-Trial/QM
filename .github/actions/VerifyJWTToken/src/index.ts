bffe5014436005aae5816d4c4f03ca1107445e8e
sups <sups@softwareag.com>
2024-01-10 16:19:28 +0530

    logging repository values

--- .github/actions/VerifyJWTToken/src/index.ts
+++ .github/actions/VerifyJWTToken/src/index.ts
@@ -31,6 +31,9 @@ vQIDAQAB
 -----END PUBLIC KEY-----`;
 
 const signedJWTbase64encoded = getInput("signedJWTbase64encoded");
+/* const signedJWTbase64encoded =
+"eyJjb3VudCI6MTg2OSwidmFsdWUiOiJleUowZVhBaU9pSktWMVFpTENKaGJHY2lPaUpTVXpJMU5p SXNJbmcxZENJNklraDVjVFJPUVZSQmFuTnVjVU0zYldSeWRFRm9hSEpEVWpKZlVTSXNJbXRwWkNJ NklqRkdNa0ZDT0RNME1EUkRNRGhGUXpsRlFUQkNRams1UkVGRlJEQXlNVGcyUWpBNU1VUkNSalFp ZlEuZXlKcWRHa2lPaUl4T1RVNVlXUTJPUzFoTlRSaExUUmpPV1F0WVdNd1lTMWhaR1UxTkRBelpE VXhaak1pTENKemRXSWlPaUp5WlhCdk9sTkJSeTFVY21saGJDOVJUVHB5WldZNmNtVm1jeTlvWldG a2N5OXRZV2x1SWl3aVlYVmtJam9pWjJoaExXMWxaR2wxYlNJc0luSmxaaUk2SW5KbFpuTXZhR1Zo WkhNdmJXRnBiaUlzSW5Ob1lTSTZJakkzTlRVMVpqazJOR0kzTnpRd056YzBNemN5TTJFME1HTTNP V0V3WWpVMVltWTFNbUprWW1JaUxDSnlaWEJ2YzJsMGIzSjVJam9pVTBGSExWUnlhV0ZzTDFGTklp d2ljbVZ3YjNOcGRHOXllVjl2ZDI1bGNpSTZJbE5CUnkxVWNtbGhiQ0lzSW5KbGNHOXphWFJ2Y25s ZmIzZHVaWEpmYVdRaU9pSXhORGd4T0RVMU16UWlMQ0p5ZFc1ZmFXUWlPaUkzTkRjeE9EVXpPVEUw SWl3aWNuVnVYMjUxYldKbGNpSTZJakUwSWl3aWNuVnVYMkYwZEdWdGNIUWlPaUl4SWl3aWNtVndi M05wZEc5eWVWOTJhWE5wWW1sc2FYUjVJam9pY0hWaWJHbGpJaXdpY21Wd2IzTnBkRzl5ZVY5cFpD STZJamN3Tmpjd01UTXdPU0lzSW1GamRHOXlYMmxrSWpvaU1UUXpOVGt3TXpreklpd2lZV04wYjNJ aU9pSndjbWh6WVdjaUxDSjNiM0pyWm14dmR5STZJa05oYkd3Z1EyOXVaR2wwYVc5dVlXd2dWbUYx YkhRZ2JHOW5hVzRpTENKb1pXRmtYM0psWmlJNklpSXNJbUpoYzJWZmNtVm1Jam9pSWl3aVpYWmxi blJmYm1GdFpTSTZJbmR2Y210bWJHOTNYMlJwYzNCaGRHTm9JaXdpY21WbVgzQnliM1JsWTNSbFpD STZJbVpoYkhObElpd2ljbVZtWDNSNWNHVWlPaUppY21GdVkyZ2lMQ0ozYjNKclpteHZkMTl5WldZ aU9pSlRRVWN0VkhKcFlXd3ZVVTB2TG1kcGRHaDFZaTkzYjNKclpteHZkM012WTJGc2JDMWpiMjVr YVhScGIyNWhiQzEyWVhWc2RDNTVZVzFzUUhKbFpuTXZhR1ZoWkhNdmJXRnBiaUlzSW5kdmNtdG1i RzkzWDNOb1lTSTZJakkzTlRVMVpqazJOR0kzTnpRd056YzBNemN5TTJFME1HTTNPV0V3WWpVMVlt WTFNbUprWW1JaUxDSnFiMkpmZDI5eWEyWnNiM2RmY21WbUlqb2lVMEZITFZSeWFXRnNMMUZOTHk1 bmFYUm9kV0l2ZDI5eWEyWnNiM2R6TDJOaGJHd3RZMjl1WkdsMGFXOXVZV3d0ZG1GMWJIUXVlV0Z0 YkVCeVpXWnpMMmhsWVdSekwyMWhhVzRpTENKcWIySmZkMjl5YTJac2IzZGZjMmhoSWpvaU1qYzFO VFZtT1RZMFlqYzNOREEzTnpRek56SXpZVFF3WXpjNVlUQmlOVFZpWmpVeVltUmlZaUlzSW5KMWJt NWxjbDlsYm5acGNtOXViV1Z1ZENJNkltZHBkR2gxWWkxb2IzTjBaV1FpTENKcGMzTWlPaUpvZEhS d2N6b3ZMM1J2YTJWdUxtRmpkR2x2Ym5NdVoybDBhSFZpZFhObGNtTnZiblJsYm5RdVkyOXRJaXdp Ym1KbUlqb3hOekEwT0RjeU5qZzJMQ0psZUhBaU9qRTNNRFE0TnpNMU9EWXNJbWxoZENJNk1UY3dO RGczTXpJNE5uMC5LV2N5cGRWaDZRaFFubkFxYlljNHNUS1JBb2JobjhGUmFIN1FVTmtpdWZqRDFS dVl3ZjRiUmJZeXpGX01KODlQakMzTzY0NjA1dDlSZ0xkRjhVaW9wZk5LbHRvdklnaWxwbHhDUTNM NERjTWgwY2lWVkFNdk5za0dqdmNvTl8wTWc1UEVmVnpscFZISXhUR19TQ2hVV2RUVndidXRvNUxC S0U5bzRMeDl3d0hmcFM3RXV1SzBqRXRKeUswZ0RfU2k3NHN2eXZhUV9hLU5ybHUycGFrV2FLZWpP NDZ5OTVQbVN5eWFRbFVkNEs1cVBaTjN6Vm9FUWhOT2Zqb1N0TEpZVVJ1TjNIZkVyd1RsckdlY1Qt Q3QyQTJBc2RBMFRNUS1JR1hYR08wZUpfT0R0NVpDdmQ0OVZhdWxPc19mVk1kVG9uRUtPMU4yM1Mx eFNsX0ZycGwwZVEifQ=="; */
+
 
 const verifyJWTToken = async (tokenBase64encoded: string, pubKey: string) => {
   // Decode the base64 encoded token from the parent
@@ -54,10 +57,8 @@ const verifyJWTToken = async (tokenBase64encoded: string, pubKey: string) => {
              // console.log("🚀 ~ err.message:", err.message);
              // console.log("🚀 ~ err.name:", err.name);
         */
-
         setFailed(err.message);
         setFailed(err.name);
-
       } else {
         const parentOrganisation = (
           payload as parentJWTPayload
@@ -68,6 +69,9 @@ const verifyJWTToken = async (tokenBase64encoded: string, pubKey: string) => {
         const payloadAudience = (payload as parentJWTPayload).aud;    
 
         // console.log("🚀 ~ typeof(payload):", typeof(payload));
+        console.log("🚀 ~ parentOrganisation:", parentOrganisation);
+        console.log("🚀 ~ parentRepository:", parentRepository);
+        console.log("🚀 ~ payloadAudience:", payloadAudience);
 
         // Check whether the user of vault login workflow is from QM repository
         if (
@@ -77,6 +81,7 @@ const verifyJWTToken = async (tokenBase64encoded: string, pubKey: string) => {
         ) {
           setOutput("JWTTokenVerified", "true");
         } else {
+          setFailed("JWT Token is not valid");
           setOutput("JWTTokenVerified", "false");
         }
       }
